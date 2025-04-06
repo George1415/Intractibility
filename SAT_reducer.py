341e25aedddf2aa15ff822a952aa518e552d5a98
George-Koumellis <11gkoumellis@brightoncollege.net>
2025-04-06 20:07:36 +0100

    SAT_reducer finished

--- SAT_reducer.py
+++ SAT_reducer.py
@@ -1,5 +1,9 @@
 clauses = [
-    [1, 2, 3, 4, 5, 6]
+    [1],
+    [1,2],
+    [1,2,3,],
+    [1,2,3,4],
+    [1,2]
 ]
 new_literal = 849
 
@@ -11,7 +15,18 @@ clauses_3SAT = []
 
 for clause in clauses:
     k = len(clause)
-    if k > 3:
+    if k == 1:
+        for a in [1, -1]:
+            for b in [1, -1]:
+                clauses_3SAT.append([clause[0], a * new_literal, b * (new_literal + 1)])
+        new_literal += 2
+    elif k == 2:
+        clauses_3SAT.append([clause[0], clause[1], new_literal])
+        clauses_3SAT.append([clause[0], clause[1], -new_literal])
+        new_literal += 1
+    elif k == 3:
+        clauses_3SAT.append(clause)
+    elif k > 3:
         for i in range(k-3):
             if i == 0:
                 clauses_3SAT.append([clause[i], clause[i+1], new_literal])
@@ -19,8 +34,7 @@ for clause in clauses:
                 clauses_3SAT.append([-(new_literal-1), clause[i+1], new_literal])
             
             new_literal += 1
-        new_literal -= 1
-        clauses_3SAT.append([-(new_literal), clause[-2], clause[-1]])
+        clauses_3SAT.append([-(new_literal-1), clause[-2], clause[-1]])
 
 expression = ""        
 for clause in clauses_3SAT:
