1a2ecdc3f41088f4dc438044825dfd133fd9e591
George-Koumellis <11gkoumellis@brightoncollege.net>
2025-04-06 20:19:08 +0100

    Improved readability

--- SAT_reducer.py
+++ SAT_reducer.py
@@ -15,17 +15,24 @@ clauses_3SAT = []
 
 for clause in clauses:
     k = len(clause)
+    # Introduce two variables and ensure their assignment has no effect on satisfiability through 4 clauses of each combination
     if k == 1:
         for a in [1, -1]:
             for b in [1, -1]:
                 clauses_3SAT.append([clause[0], a * new_literal, b * (new_literal + 1)])
         new_literal += 2
+    
+    # Introduce a variable and ensure their assignment has no effect on satisfiability through a clause for both its value and negation
     elif k == 2:
         clauses_3SAT.append([clause[0], clause[1], new_literal])
         clauses_3SAT.append([clause[0], clause[1], -new_literal])
         new_literal += 1
+    
+    # Case already satisfied
     elif k == 3:
         clauses_3SAT.append(clause)
+
+    # Progressively split clause
     elif k > 3:
         for i in range(k-3):
             if i == 0:
